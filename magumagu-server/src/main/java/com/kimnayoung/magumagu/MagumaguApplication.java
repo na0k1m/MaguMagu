d0f6fc39ea8c1a9955fd2182c355e8355921c95f
Nayoung.Kim <125175518+na0k1m@users.noreply.github.com>
2026-08-13 17:06:20 +0900

    chore: 프로젝트 구조 세팅

---
+++ magumagu-server/src/main/java/com/kimnayoung/magumagu/MagumaguApplication.java
@@ -0,0 +1,13 @@
+package com.kimnayoung.magumagu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagumaguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagumaguApplication.class, args);
+	}
+
+}
---
+++ magumagu-server/src/test/java/com/kimnayoung/magumagu/MagumaguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimnayoung.magumagu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagumaguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
