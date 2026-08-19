a8e394b46f85a033d5fa643a8f56724ccbf77005
Nayoung.Kim <125175518+na0k1m@users.noreply.github.com>
2026-08-19 15:04:47 +0900

    chore: macOS 프로젝트 파일 생성 및 ignore 세팅

---
+++ magumagu-macOS/magumagu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  magumagu
+//
+//  Created by 김나영 on 8/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ magumagu-macOS/magumagu/magumaguApp.swift
@@ -0,0 +1,17 @@
+//
+//  magumaguApp.swift
+//  magumagu
+//
+//  Created by 김나영 on 8/19/26.
+//
+
+import SwiftUI
+
+@main
+struct magumaguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
